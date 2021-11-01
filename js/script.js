51a0d9b2738c446a2bec2675636f5a9f2eef8768
Alef Freitas <alef.freitas07@gmail.com>
2021-11-01 15:21:16 -0300

    Adicionado inputs e iniciado lógica de programação

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let quantidadeAdultos = document.getElementById('adultos');
+let quantidadeCriancas = document.getElementById('criancas');
+let duracaoChurrasco = document.getElementById('duracao');
+
