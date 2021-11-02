036333ed3921910daecc71b85642448c7f33ce83
Alef Freitas <alef.freitas07@gmail.com>
2021-11-02 20:19:13 -0300

    Finalizado estilização web

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let quantidadeAdultos = document.getElementById('totalAdultos');
 let quantidadeCriancas = document.getElementById('totalCriancas');
 let duracaoChurrasco = document.getElementById('duracao');
+document.getElementById('final').style.display = 'none';
 
 function calcular(){
     
@@ -11,7 +12,7 @@ function calcular(){
     let crianca = Number(quantidadeCriancas.value) * 0.5;
     let adulto = Number(quantidadeAdultos.value);    
 
-    if(crianca == 0 || adulto == 0 || duracaoChurrasco.value == 0){
+    if(crianca < 0 || adulto == "" || duracaoChurrasco.value == 0){
         alert('Preencha os campos antes de calcular.')
     }else{        
 
@@ -24,10 +25,12 @@ function calcular(){
         carne *= (crianca + adulto);
         cerveja *= adulto;
         refriAgua *= (crianca + adulto);
-
+        
         document.getElementById('totalCarne').innerHTML = `${Math.ceil(carne)} Kg de Carne`;
-        document.getElementById('totalCerveja').innerHTML = `${Math.ceil((cerveja/350)).toFixed(0)} latinhas de Cerveja`;
-        document.getElementById('totalRefri').innerHTML = `${Math.ceil((refriAgua/2))} garrafas de 2L de Refrigerante`;
+        document.getElementById('totalCerveja').innerHTML = `${Math.ceil((cerveja/350))} latinhas de Cerveja`;
+        document.getElementById('totalRefri').innerHTML = `${Math.ceil((refriAgua/2))} garrafas de 2L de Refri`;
         document.getElementById('totalAgua').innerHTML = `${Math.ceil((refriAgua/2))} garrafas de 2L de Água`;
+
+        document.getElementById('final').style.display = '';
     }
 }
